3a7a48fee4e99ecf01e6239e3fb171551fe9e15c
Raman Bhise <ramanbhise69@gmail.com>
2024-09-07 02:25:29 +0530

    new file:   index.html new file:   script.js new file:   styles.css

---
+++ script.js
@@ -0,0 +1,5 @@
+// Scroll to the projects section when the "View Projects" button is clicked
+function scrollToProjects() {
+    const projectsSection = document.getElementById('projects');
+    projectsSection.scrollIntoView({ behavior: 'smooth' });
+}
